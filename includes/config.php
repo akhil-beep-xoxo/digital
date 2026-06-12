d5ae0a2cf067856435da65e4c54b104d1c860a69
akhil-beep-xoxo <ganjaakhil@gmail.com>
2026-06-12 13:01:46 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,18 +1,6 @@
 <?php
-$host = getenv('MYSQLHOST');
-$dbname = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname",
-        $user,
-        $password
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
-?>
+$dbFile = __DIR__ . '/../database.sqlite';
+
+$pdo = new PDO('sqlite:' . $dbFile);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
